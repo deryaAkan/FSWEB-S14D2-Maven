5eab177da442023941319642b80b94457afc911a
Derya Akan <d.akan369@gmail.com>
2024-04-04 16:26:20 +0300

    Tasks done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,20 @@
 package org.example;
 
+import org.example.model.Bed;
+import org.example.model.Bedroom;
+import org.example.model.Ceiling;
+import org.example.model.enums.PaintColor;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Bedroom bedroom = new Bedroom();
+        bedroom.setName("Master Bedroom");
+        bedroom.setBed(new Bed("Lovely", 3, 5, 6, 5));
+        bedroom.setCeiling(new Ceiling(175, PaintColor.GREEN));
+        System.out.println("Bedroom Name: " + bedroom.getName());
+        System.out.println("Bed: " + bedroom.getBed());
+        System.out.println("****************");
+        System.out.println("Ceiling: " + bedroom.getCeiling());
+
     }
 }
